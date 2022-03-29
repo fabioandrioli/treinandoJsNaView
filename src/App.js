99d0fbcfd94d26373ad8f14986ee61b2f51e4a72
Fabio Gilberto <fabio.goncalves@bomjesus.br>
2022-03-29 08:55:57 -0300

    App front de tarefas criado com sucesso

--- src/App.js
+++ src/App.js
@@ -7,26 +7,58 @@ const App = {
   timer:0,
   tasks:[
     {
+      id: 1,
       title:"Criar um cronometro para saber quanto tempo gasto em cada tarefa",
-      time:"01:28:35",
+      time:{
+        minutes:0,
+        seconds:0,
+        hours:0,
+      },
       date:"25/03/2022",
     },
     {
+      id: 2,
       title:"Criar um cronometro para saber quanto tempo gasto em cada tarefa",
-      time:"01:28:35",
+      time:{
+        minutes:0,
+        seconds:0,
+        hours:0,
+      },
       date:"25/03/2022",
     },
     {
+      id: 3,
       title:"Criar um cronometro para saber quanto tempo gasto em cada tarefa",
-      time:"01:28:35",
+      time:{
+        minutes:0,
+        seconds:0,
+        hours:0,
+      },
       date:"25/03/2022",
     }
   ],
 
+  newTask(){
+    timer = App.calculateAndTransformTimeForHoursMinutesSeconds();
+    let task = {
+      id: this.tasks.length + 1,
+      title: this.inputTextTask.value,
+      time:{
+        minutes:timer.minutes,
+        seconds:timer.seconds,
+        hours:timer.hours,
+      },
+      date: new Date().toLocaleDateString(),
+    }
+    this.tasks.push(task);
+  },
+
   init(){
+    console.log(App.tasks)
+    App.view.addTaskInTable(App.tasks);
     App.btnStart.addEventListener('click',App.buttons.initTimer);
     App.btnPause.addEventListener('click',App.buttons.pauseTimer);
-    App.btnFinish.addEventListener('click',App.buttons.finishTimer);
+    App.btnFinish.addEventListener('click',App.buttons.finishTimer);  
   },
 
   validateInitTask(){
@@ -66,12 +98,46 @@ const App = {
       warning = document.createElement('div');
       warning.innerHTML = ` <div id="warning" class="box-warning box-warning-show">Por favor, insira uma tarefa!</div>`;
       body.insertBefore(warning, body.firstChild);
+      warning = document.querySelector('.box-warning-show');
       setTimeout(() => {
-        warning = document.querySelector('.box-warning-show');
+        warning.classList.remove('box-warning-show');
         warning.classList.add('box-warning-hidden');
         document.getElementById('warning').remove();
-      }, 2000);
+      }, 1000);
     
+    },
+
+    createButtonDelete(task){
+      let button = document.createElement('button');
+      button.classList.add('btn','btn-danger');
+      button.classList.add('delete');
+      button.style.margin = '5px 0'
+      button.innerHTML = 'deletar';
+      button.dataset.id = `${task.id}`;
+      return button;
+    },
+
+    addTaskInTable(){
+      let table = document.querySelector('#table_tasks tbody');
+      table.innerHTML = '';
+      App.tasks.forEach(task => {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        tr.innerHTML = `
+        <th scope="row">${task.id}</th>
+        <td>${task.title}</td>
+        <td>${App.formatTime(task.time.hours)}:${App.formatTime(task.time.minutes)}:${App.formatTime(task.time.seconds)}</td>
+        <td>${task.date}</td>
+        `;
+        td.appendChild(App.view.createButtonDelete(task));
+        tr.appendChild(td);
+        table.insertBefore(tr,table.firstChild);
+      });
+      App.buttons.buttonDelete();
+    },
+
+    resetIpunts(){
+      App.inputTextTask.value = '';
     }
   },
  
@@ -94,10 +160,13 @@ const App = {
 
     finishTimer(){
       clearInterval(interval)
+      App.newTask();
       App.buttons.activityButton([App.btnStart]);
       App.buttons.disabledButton([App.btnPause,App.btnFinish]);
       App.resetTimer();
       App.view.showTimer(App.calculateAndTransformTimeForHoursMinutesSeconds());
+      App.view.addTaskInTable();
+      App.view.resetIpunts();
     },
 
     disabledButton(buttons){
@@ -107,6 +176,19 @@ const App = {
     activityButton(buttons){
       buttons.forEach(button => button.removeAttribute("disabled"));
     },
+
+    buttonDelete(){
+      let buttons = document.querySelectorAll('.delete');
+      buttons.forEach(button => button.addEventListener('click',() => {App.deleteTask(button.dataset.id)}));
+     
+    },
+  },
+
+  deleteTask(task){
+    console.log(task)
+    App.tasks = App.tasks.filter(t => t.id != task);
+    console.log(App.tasks)
+    App.view.addTaskInTable();
   },
 
   song:{
